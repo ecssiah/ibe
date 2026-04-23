2b4af0a5cd9e746a519334530c8feb3e34df30bb
ecssiah <ecssiah@gmail.com>
2026-04-23 19:47:46 +0800

    add facing to character

--- src/bin/ibe000.rs
+++ src/bin/ibe000.rs
@@ -15,6 +15,7 @@ fn main() {
 #[derive(Component)]
 struct Character {
     speed: f32,
+    facing: i32,
 }
 
 fn setup_character(
@@ -26,7 +27,10 @@ fn setup_character(
     let sprite_image_handle = asset_server.load("textures/rpg/chars/hat-guy/hat-guy.png");
 
     commands.spawn((
-        Character { speed: 256.0 }, 
+        Character {
+            speed: 256.0,
+            facing: 0,
+        }, 
         Sprite::from_image(sprite_image_handle),
         Transform::from_xyz(0., 0., 0.)
             .with_scale(Vec3::splat(6.0))
@@ -36,26 +40,28 @@ fn setup_character(
 fn move_character(
     time: Res<Time>, 
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<(&mut Transform, &mut Sprite, &Character)>
+    mut query: Query<(&mut Transform, &mut Sprite, &mut Character)>
 ) {
-    let mut input_value = 0.0;
+    for (mut transform, mut sprite, mut character) in &mut query {
+        let mut input_value = 0.0;
 
-    if keyboard_input.pressed(KeyCode::KeyD) {
-        input_value += 1.0;
-    }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            input_value += 1.0;
+        }
 
-    if keyboard_input.pressed(KeyCode::KeyA) {
-        input_value -= 1.0;
-    }
-
-    for (mut transform, mut sprite, character) in &mut query {
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            input_value -= 1.0;
+        }
+        
         let displacement = input_value * character.speed * time.delta_secs();
 
         transform.translation.x += displacement;
 
         if input_value > 0.0 {
+            character.facing = 1;
             sprite.flip_x = false;
         } else if input_value < 0.0 {
+            character.facing = -1;
             sprite.flip_x = true;
         }
     }
